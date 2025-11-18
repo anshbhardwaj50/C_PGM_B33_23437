0d5539c711a3c81bd9bb0284fe1ccb777f532d79
anshbhardwaj50 <anshbhardwaj00001@gmail.com>
2025-11-18 08:42:26 +0530

    Implement basic addition program in C

---
+++ 1_4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main (){
+
+int a,b,c;                 //variables for numbers and result.
+
+printf("Enter your First Number: "); //Take first number from user
+scanf("%d",&a);                       //Reads the first integer
+printf("Enter Your Second No.: ");   //Take second number from user
+scanf("%d",&b);                      //Reads the second integer
+c=a+b;                               //Addition
+
+printf("Addition:%d",c);             //Display Result
+    return 0;
+}
